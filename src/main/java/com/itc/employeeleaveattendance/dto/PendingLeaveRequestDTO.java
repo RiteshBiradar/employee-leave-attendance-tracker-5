78c7b81dd13eb212daf0b33d88e7307f11870769
BOGUDA JAYAVARDHAN <bogudajayavardhan@BOGUDAs-MacBook-Air.local>
2026-09-06 14:38:42 +0530

    Add pending leave request DTO

---
+++ src/main/java/com/itc/employeeleaveattendance/dto/PendingLeaveRequestDTO.java
@@ -0,0 +1,91 @@
+package com.itc.employeeleaveattendance.dto;
+
+import java.time.LocalDate;
+
+public class PendingLeaveRequestDTO {
+
+    private int requestId;
+    private int empId;
+    private String employeeName;
+    private String leaveType;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private int workingDays;
+    private String reason;
+    private String status;
+
+    public PendingLeaveRequestDTO() {
+    }
+
+    public int getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(int requestId) {
+        this.requestId = requestId;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getLeaveType() {
+        return leaveType;
+    }
+
+    public void setLeaveType(String leaveType) {
+        this.leaveType = leaveType;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getWorkingDays() {
+        return workingDays;
+    }
+
+    public void setWorkingDays(int workingDays) {
+        this.workingDays = workingDays;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
